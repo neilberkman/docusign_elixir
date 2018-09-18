e1cbbf7246a89e80be5fccb75d31425ec500b041
Maksim Pechnikov <parallel588@gmail.com>
2018-09-18 13:35:42 +0300

    add User struct

---
+++ lib/docusign/user.ex
@@ -0,0 +1,39 @@
+defmodule DocuSign.User do
+  @moduledoc """
+  The module contains struct of User and functions for work it.
+
+  ### Examples
+
+  client = DocuSign.OAuth2Strategy.get_token!
+  user_info = DocuSign.User.info(client)
+
+  """
+
+  defstruct [
+    :sub, #The user ID of the account holder
+    :name, #The name of the account holder
+    :given_name, #The given name of the account holder
+    :family_name, #The family name of the account holder
+    :created, #The DateTime when the account was created
+    :email, #The email address of the account holder
+    :accounts, #Holds data on the individual accounts owned by the user. Each user may possess multiple accounts.
+    :account_id, #The ID of the account.
+    :is_default, #If true, this account is the account holder's default account.
+    :account_name, #The name associated with this account.
+    :base_uri, #The base URI that is used for making API calls on behalf of this account.
+    :organization, #Holds data on the organization to which this account belongs.
+    :organization_id, #The ID of the organization to which this account belongs.
+    :links #Holds a link to the organization's base URL.
+  ]
+
+  alias OAuth2.Client
+  alias DocuSign.Util
+
+  @path "/oauth/userinfo"
+
+  def info(client) do
+    with {:ok, %{body: body}} <- Client.get(client, @path),
+         attrs <- Util.map_keys_to_atoms(body),
+      do: struct(__MODULE__, attrs)
+  end
+end
---
+++ lib/docusign/util.ex
@@ -0,0 +1,42 @@
+defmodule DocuSign.Util do
+  @moduledoc false
+
+  @doc """
+  Performs a root-level conversion of map keys from strings to atoms.
+
+  This function performs the transformation safely using `String.to_existing_atom/1`, but this has a possibility to raise if
+  there is not a corresponding atom.
+
+  It is recommended that you pre-filter maps for known values before
+  calling this function.
+
+  ## Examples
+
+  iex> map = %{
+  ...>   "a"=> %{
+  ...>     "b" => %{
+  ...>       "c" => 1
+  ...>     }
+  ...>   }
+  ...> }
+  iex> DocuSign.Util.map_keys_to_atoms(map)
+  %{
+  a: %{
+  "b" => %{
+  "c" => 1
+  }
+  }
+  }
+  """
+  def map_keys_to_atoms(m) do
+    Enum.into(m, %{}, fn
+      {k, v} when is_binary(k) ->
+        a = String.to_existing_atom(k)
+      {a, v}
+
+      entry ->
+        entry
+    end)
+  end
+
+end
