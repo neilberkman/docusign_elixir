db89900a33172f062d3e6b2832786e1f7f3e13cb
Maksim Pechnikov <parallel588@gmail.com>
2018-09-18 13:58:48 +0300

    init test

---
+++ test/docusign/ouath2_strategy_test.exs
@@ -0,0 +1,11 @@
+defmodule DocuSign.OAuth2StrategyTest do
+  use ExUnit.Case
+  alias DocuSign.OAuth2Strategy
+  alias OAuth2.{Client, AccessToken}
+  test "token_expired?" do
+    token = %AccessToken{expires_at: :os.system_time(:seconds) - 30}
+    assert OAuth2Strategy.token_expired?(nil)
+    assert OAuth2Strategy.token_expired?(token)
+    assert OAuth2Strategy.token_expired?(%Client{token: token})
+  end
+end
