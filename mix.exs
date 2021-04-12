42a2334d76621758724e7585e64b3870c14fbda3
Neil Berkman <neil@berkman.com>
2021-04-12 10:40:24 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DocuSign.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.3.2"
   @url "https://github.com/neilberkman/docusign_elixir"
   @maintainers [
     "Neil Berkman"
