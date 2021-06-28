78b8b3a691be75aea1903652f2b82245346c3f53
Hugo Frappier <hfrappier@givernycapital.com>
2021-06-28 16:37:03 -0400

    feat: Add simple client registry
    To keep track of multiple clients using different user IDs.

--- test/docusign/api_client_test.exs
+++ test/docusign/api_client_test.exs
@@ -34,13 +34,52 @@ defmodule DocuSign.APIClientTest do
     test "returns client for given user ID" do
       @oauth_mock
       |> expect(:client, fn opts ->
-        assert opts[:user_id] == ":another-user-id:"
+        assert opts[:user_id] == ":other-user-id:"
         %OAuth2.Client{}
       end)
       |> expect(:refresh_token!, fn client, _force -> client end)
       |> expect(:interval_refresh_token, fn _client -> 1000 end)
 
-      APIClient.client(":another-user-id:", oauth_impl: @oauth_mock)
+      APIClient.client(":other-user-id:", oauth_impl: @oauth_mock)
+    end
+
+    test "2 user_id returns 2 clients" do
+      @oauth_mock
+      |> expect(:client, 2, fn opts ->
+        assert opts[:user_id] in [":user-id:", ":other-user-id:"]
+        %OAuth2.Client{ref: %{user_id: opts[:user_id]}}
+      end)
+      |> expect(:refresh_token!, 2, fn client, _force -> client end)
+      |> expect(:interval_refresh_token, 2, fn _client -> 1000 end)
+
+      client_1 = APIClient.client(":user-id:", oauth_impl: @oauth_mock)
+      client_2 = APIClient.client(":other-user-id:", oauth_impl: @oauth_mock)
+
+      assert %OAuth2.Client{ref: %{user_id: ":user-id:"}} = client_1
+      assert %OAuth2.Client{ref: %{user_id: ":other-user-id:"}} = client_2
+    end
+  end
+
+  describe "refreshing access token" do
+    test "client access token is refreshed after delay" do
+      # 2 refreshes when clients are created, and 1 when a client gets refreshed
+      expected_force_token_refresh_count = 3
+
+      @oauth_mock
+      |> expect(:client, 2, fn opts -> %OAuth2.Client{ref: %{user_id: opts[:user_id]}} end)
+      |> expect(:refresh_token!, expected_force_token_refresh_count, fn client, true -> client end)
+      |> expect(:interval_refresh_token, 3, fn %{ref: %{user_id: user_id}} ->
+        case user_id do
+          ":user-id:" -> 1
+          _ -> 60
+        end
+      end)
+
+      _client_to_refresh = APIClient.client(":user-id:", oauth_impl: @oauth_mock)
+      _client_not_to_refresh = APIClient.client(":other-user-id:", oauth_impl: @oauth_mock)
+
+      # Wait for refresh to occur (1sec)
+      Process.sleep(1_500)
     end
   end
 end
