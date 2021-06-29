46a4537221d80098bd95dab5a774b464ef5e56e6
Hugo Frappier <hfrappier@givernycapital.com>
2021-06-29 15:35:18 -0400

    refactor: Avoid the use of Mix.env in production code

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
+config :docusign, app_env: :test
 config :docusign, hostname: "account-d.docusign.com"
 config :docusign, private_key: "test/support/test_key"
 config :docusign, user_id: ":user-id:"
