cc966289090b1bd585d9302afabfc2160df6c0a1
Hugo Frappier <hfrappier@givernycapital.com>
2021-06-29 16:10:47 -0400

    refactor: Simplify code by passing OAuth implementation when starting gen server

--- test/docusign/connection_test.exs
+++ test/docusign/connection_test.exs
@@ -17,7 +17,7 @@ defmodule DocuSign.ConnectionTest do
 
   describe "creating a connection for default user" do
     setup do
-      {:ok, pid} = DocuSign.ClientRegistry.start_link()
+      {:ok, pid} = DocuSign.ClientRegistry.start_link(oauth_impl: DocuSign.OAuth.Fake)
       on_exit(fn -> assert_down(pid) end)
     end
 
@@ -31,7 +31,7 @@ defmodule DocuSign.ConnectionTest do
 
   describe "getting a new connection for a user ID" do
     setup do
-      {:ok, pid} = DocuSign.ClientRegistry.start_link()
+      {:ok, pid} = DocuSign.ClientRegistry.start_link(oauth_impl: @oauth_mock)
       on_exit(fn -> assert_down(pid) end)
     end
 
@@ -44,7 +44,7 @@ defmodule DocuSign.ConnectionTest do
       |> expect(:refresh_token!, fn client, _force -> client end)
       |> expect(:interval_refresh_token, fn _client -> 1000 end)
 
-      connection = Connection.get(":other-user-id:", oauth_impl: @oauth_mock)
+      connection = Connection.get(":other-user-id:")
 
       assert connection.client.ref.user_id == ":other-user-id:"
     end
