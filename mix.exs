4dcb6e5643dfd5fda3067765ca4f037ec0fc895a
Hugo Frappier <hfrappier@givernycapital.com>
2021-06-29 14:19:59 -0400

    feat: Deprecate Connection.new/0
    Connection.get/1 should be used instead.

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DocuSign.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.3.4"
+  @version "0.4.0"
   @url "https://github.com/neilberkman/docusign_elixir"
   @maintainers [
     "Neil Berkman"
