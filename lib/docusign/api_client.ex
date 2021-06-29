45af59af6dea3b1416fa6f95e7aa9bd8e1ed696b
Hugo Frappier <hfrappier@givernycapital.com>
2021-06-29 14:23:18 -0400

    feat: Add back APIClient for backward compatibility
    And deprecate those functions.

---
+++ lib/docusign/api_client.ex
@@ -0,0 +1,29 @@
+defmodule DocuSign.APIClient do
+  @moduledoc ~S"""
+  Deprecated interface to manage OAuth2 clients. `DocuSign.ClientRegistry` should be used
+  instead.
+  """
+  alias DocuSign.ClientRegistry
+
+  @doc """
+  Get Api Client
+  """
+  @deprecated "Use DocuSign.ClientRegistry.client/1 instead."
+  @spec client() :: OAuth2.Client.t()
+  def client do
+    ClientRegistry.client(default_user_id())
+  end
+
+  @doc """
+  Forces an access token refresh.
+  """
+  @deprecated "Use DocuSign.ClientRegistry.refresh_token/1 instead."
+  @spec refresh_token() :: OAuth2.Client.t()
+  def refresh_token do
+    # ClientRegistry.refresh_token(default_user_id())
+  end
+
+  defp default_user_id do
+    Application.fetch_env!(:docusign, :user_id)
+  end
+end
