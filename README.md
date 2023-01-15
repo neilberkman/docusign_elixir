00cb7ea7be593d1efc07451e34c0709be9e88185
Neil Berkman <neil@berkman.com>
2023-01-14 18:08:02 -0800

    bump version

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The package can be installed by adding `docusign` to your list of dependencies i
 ```elixir
 def deps do
   [
-    {:docusign, "~> 0.4.4"}
+    {:docusign, "~> 1.0.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DocuSign.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.4.4"
+  @version "1.0.0"
   @url "https://github.com/neilberkman/docusign_elixir"
   @maintainers [
     "Neil Berkman"
