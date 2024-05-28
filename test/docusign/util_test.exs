c8e07bdf0df5f16c01cfaa4c818194bb16804dd9
Neil Berkman <neil@xuku.com>
2024-05-27 21:56:55 -0700

    Update dependencies

--- test/docusign/util_test.exs
+++ test/docusign/util_test.exs
@@ -2,7 +2,7 @@ defmodule DocuSign.UtilTest do
   use ExUnit.Case
   import DocuSign.Util
 
-  test 'map_keys_to_atoms' do
+  test ~c"map_keys_to_atoms" do
     assert map_keys_to_atoms(%{"test" => 1}) == %{test: 1}
   end
 end
