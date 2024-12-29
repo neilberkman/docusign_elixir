c42db45800425e3ebf84ec246927738717615059
Neil Berkman <neil@xuku.com>
2024-12-29 09:37:03 -0800

    correct type specs and add dialyzer configuration
    - Update @specs in APIClient and Connection to match actual returns
    - Add dialyzer config to handle generated code warnings

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,3 @@
+[
+  ~r/lib\/docusign\/api\/.*/
+]
--- lib/docusign/api_client.ex
+++ lib/docusign/api_client.ex
@@ -3,13 +3,14 @@ defmodule DocuSign.APIClient do
   Deprecated interface to manage OAuth2 clients. `DocuSign.ClientRegistry` should be used
   instead.
   """
+
   alias DocuSign.ClientRegistry
 
   @doc """
   Get Api Client
   """
   @deprecated "Use DocuSign.ClientRegistry.client/1 instead."
-  @spec client() :: OAuth2.Client.t()
+  @spec client() :: {:ok, OAuth2.Client.t()} | {:error, any()}
   def client do
     ClientRegistry.client(default_user_id())
   end
@@ -18,7 +19,7 @@ defmodule DocuSign.APIClient do
   Forces an access token refresh.
   """
   @deprecated "Use DocuSign.ClientRegistry.refresh_token/1 instead."
-  @spec refresh_token() :: OAuth2.Client.t()
+  @spec refresh_token() :: {:ok, OAuth2.Client.t()} | {:error, any()}
   def refresh_token do
     # TO BE IMPLEMENTED
     # ClientRegistry.refresh_token(default_user_id())
