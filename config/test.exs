3b71ae7fe2582dc0db5f8e8e05a20a50c37597c0
Neil Berkman <neil@xuku.com>
2025-03-13 07:30:35 -0700

    Remove all deprecated code for v2.0.0
    - Remove deprecated APIClient module
    - Remove User.info/0 default client behavior
    - Update config to use private_key_file consistently
    - Remove ClientRegistry alias in User module
    - Clean up implementation code

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,7 @@ import Config
 
 config :docusign, app_env: :test
 config :docusign, hostname: "account-d.docusign.com"
-config :docusign, private_key: "test/support/test_key"
+config :docusign, private_key_file: "test/support/test_key"
 config :docusign, user_id: ":user-id:"
 
 config :docusign, oauth_implementation: DocuSign.OAuth.Fake
