841a0c90d536fb753f08a3399b0cde62361f80c8
Neil Berkman <neil@xuku.com>
2025-03-13 07:36:34 -0700

    Remove cowlib dependency override

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule DocuSign.MixProject do
       {:castore, "~> 1.0"},
       {:mint, "~> 1.0"},
       {:meck, "~> 0.9.2"},
-      {:cowlib, "2.14.0", override: true},
+      {:cowlib, "2.14.0"},
 
       # test
       {:mock, "~> 0.3.2", only: :test},
