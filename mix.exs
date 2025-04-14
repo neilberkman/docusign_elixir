e08743494e76445d077c001368ecc00cd7c9d614
Neil Berkman <neil@xuku.com>
2025-04-14 13:12:12 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule DocuSign.MixProject do
       {:castore, "~> 1.0"},
       {:mint, "~> 1.0"},
       {:meck, "~> 0.9.2"},
-      {:cowlib, "2.14.0"},
+      {:cowlib, "2.15.0"},
 
       # test
       {:mock, "~> 0.3.2", only: :test},
