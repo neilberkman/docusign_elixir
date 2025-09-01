8d3c84dce8f7ebdefe778749f38d2e4f5f16efe7
Neil Berkman <neil@xuku.com>
2025-08-31 22:50:09 -0700

    Prepare for 3.0.0 release
    - Update version to 3.0.0 in mix.exs, README, and examples
    - Update LiveBooks to use released version instead of local path
    - Update CHANGELOG with complete 3.0.0 release notes
    - Document removal of ModelCleaner internal workaround
    - Fix version references in documentation examples

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DocuSign.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "2.2.4"
+  @version "3.0.0"
   @url "https://github.com/neilberkman/docusign_elixir"
   @maintainers [
     "Neil Berkman"
